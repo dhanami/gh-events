0ccad3692d6be3efe8d87828f6b9a9c35d7fbbf1
dhana <dhana.mi@outlook.com>
2024-11-04 17:49:32 +0000

    added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // mytests
+// add yet another commet
 
 describe('MainContent', () => {
   it('should render a button', () => {
